ac59a26b6346441c859c073d41e874070413e2b8
Ben Morgan <benmorgan2@gmail.com>
2011-11-09 14:03:24 +0000

    Comments generalised

--- Open Transmit Here.rb
+++ Open Transmit Here.rb
@@ -1,5 +1,5 @@
 #!/opt/local/bin/ruby
-# November 8, 2011
+# November 9, 2011
 # Script to extract server name and current path from a Terminal window, and open the same directory in Transmit
 
 require 'appscript'
@@ -8,15 +8,15 @@ terminal = Appscript.app("/Applications/Utilities/Terminal.app")
 transmit = Appscript.app("/Applications/Transmit.app")
 whose = Appscript.its
 
-window_title = terminal.windows[0].tabs[1].custom_title.get # e.g. morgan@kittel:~/TiO2-B/PBE/2Li/Li_Ca/opt/super
+window_title = terminal.windows[0].tabs[1].custom_title.get # e.g. user@host:~/Files/subdir1/subdir2
 
-server = /@([A-Za-z]*)/.match(window_title)[1] # match e.g. kittel
-path = /\:([^^]*)/.match(window_title)[1] # match e.g. ~/TiO2-B/PBE/2Li/Li_Ca/opt/super
+host = /@([A-Za-z]*)/.match( window_title )[1] # match e.g. host
+path = /\:([^^]*)/.match( window_title )[1]    # match e.g. ~/Files/subdir1/subdir2
 
 transmit.activate
-transmit.make(:new => :document)
+transmit.make( :new => :document )
 tab = transmit.documents[0].current_tab
 
-my_fave = transmit.favorites[whose.name.eq(server)].items[1].get
-tab.connect(:to => my_fave)
-tab.remote_browser.change_location(:to_path => path)
+my_fave = transmit.favorites[ whose.name.eq( host ) ].items[1].get
+tab.connect( :to => my_fave )
+tab.remote_browser.change_location( :to_path => path )
